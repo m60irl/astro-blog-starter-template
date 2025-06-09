fef0196fb1ac88e0e5bd34028fdb597b68fa5caf
m60irl <github.mf96t@a.m60.live>
2025-06-09 18:31:20 -0400

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "m60.live";
+export const SITE_DESCRIPTION = "Returning soon!";
